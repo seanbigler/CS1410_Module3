aecf45fdc09a010a04d9a9153232b7cefbd8d9ab
w01164264 <seanbigler@mail.weber.edu>
2017-09-19 13:00:15 -0600

    Add new file SeanBiglerHw2_Task1

---
+++ SeanBiglerHw2_Task1.cpp
@@ -0,0 +1,21 @@
+// File: SeanBiglerHw2_Task1
+// Created by Sean Bigler on 9/19/2017 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+// Constants
+
+
+// Prototypes
+
+
+// Main Program Program
+int main(void) {
+
+
+    return 0;
+}
+
+// Function Definitions
